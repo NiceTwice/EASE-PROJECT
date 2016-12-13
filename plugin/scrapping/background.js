f35f3d28fae9bb8a7ad7a5162cf61a3f355e9a80
felix <fel.richart@gmail.com>
2016-12-13 11:12:36 +0100

    scrap tuto

--- plugin/scrapping/background.js
+++ plugin/scrapping/background.js
@@ -1,10 +1,109 @@
-function startScrapFacebook(){
+function startScrapFacebook(login, password){
     extension.currentWindow(function(window){
-        extension.tabs.create(window, "https://www.facebook.com/settings?tab=applications", false, function(tab){
-            extension.tabs.onMessage(tab, "scrapFacebook", function(result){
-                console.log(result);
-                extension.tabs.close(tab, function(){});
+        extension.tabs.create(window, "https://www.facebook.com", false, function(tab){
+             extension.tabs.onUpdated(tab, function (newTab) {
+                tab = newTab;
+                extension.tabs.inject(tab, ["tools/extensionLight.js","overlay/overlay.css", "overlay/injectOverlay.js"], function(){});
+            });
+            extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+            //extension.tabs.onUpdated(tab, function (tab) {
                 extension.tabs.onMessageRemoveListener(tab);
+                extension.tabs.sendMessage(tab, "checkFbCo", {}, function(response){
+                    if(response==true){
+                        logoutFromFb(tab, function(tab){
+                            connectToFb(tab,login, password, scrapFb);
+                        });
+                    } else {
+                        connectToFb(tab, login, password,scrapFb);
+                    }
+                });
+            });
+        });
+    });
+}
+
+function logoutFromFb(tab, callback){
+    extension.tabs.sendMessage(tab, "logoutFromFb", {}, function(response2){
+        extension.tabs.update(tab, "https://www.facebook.com", function(tab){
+            extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+                extension.tabs.onMessageRemoveListener(tab);
+                callback(tab);
+            });
+        });
+    });
+}
+
+function connectToFb(tab, login, pass, callback){
+    extension.tabs.sendMessage(tab, "connectToFb", {login:login, pass:pass}, function(response){
+        extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+            extension.tabs.onMessageRemoveListener(tab);
+            extension.tabs.sendMessage(tab, "checkFbCo", {}, function(response){
+                if(response == false){
+                    console.log("error : wrong fb login or password");
+                } else {
+                    callback(tab);
+                }
+            });
+        });
+    });
+}
+
+function scrapFb(tab){
+    extension.tabs.update(tab, "https://www.facebook.com/settings?tab=applications", function(tab){
+        extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+            extension.tabs.onMessageRemoveListener(tab);
+            extension.tabs.sendMessage(tab, "scrapFb", {}, function(response){
+                console.log(response);
+            });
+        });
+    });
+}
+
+
+
+
+function startScrapChrome(login, password){
+    extension.currentWindow(function(window){
+        extension.tabs.create(window, "https://accounts.google.com/Logout", false, function(tab){
+             extension.tabs.onUpdated(tab, function (newTab) {
+                tab = newTab;
+                extension.tabs.inject(tab, ["tools/extensionLight.js","overlay/overlay.css", "overlay/injectOverlay.js"], function(){});
+            });
+            extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+                extension.tabs.onMessageRemoveListener(tab);
+                extension.tabs.update(tab, "https://accounts.google.com/ServiceLogin?sacu=1#identifier", function(tab){
+                    extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+                        extension.tabs.onMessageRemoveListener(tab);
+                        extension.tabs.sendMessage(tab, "connectToChrome", {login:login, pass:password}, function(response){
+                            if(response==false){
+                                console.log("cant connect to this chrome account");
+                            } else {
+                                extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+                                    extension.tabs.onMessageRemoveListener(tab);
+                                    extension.tabs.sendMessage(tab, "checkChromeCo", {}, function(isConnected){
+                                        if(!isConnected){
+                                            console.log("cant connect to this chrome account");
+                                        } else {
+                                            extension.tabs.update(tab, "https://passwords.google.com/", function(tab){
+                                                extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+                                                    extension.tabs.onMessageRemoveListener(tab);
+                                                    extension.tabs.sendMessage(tab, "typePasswordChrome", {pass:password}, function(response){
+                                                        extension.tabs.onMessage(tab, "reloaded", function (event, sendResponse1) {
+                                                            extension.tabs.onMessageRemoveListener(tab);
+                                                                extension.tabs.sendMessage(tab, "scrapChrome", {}, function(response){
+                                                                    console.log(response);
+                                                                });
+                                                            });
+                                                        });
+                                                    });
+                                                });
+                                            }
+                                    });
+                                });
+                            }
+                        });
+                    });
+                });
             });
         });
     });
--- plugin/scrapping/scrapFacebook.js
+++ plugin/scrapping/scrapFacebook.js
@@ -1,14 +1,63 @@
-function waitload(callback){
-    if($("._ikh._4na3").length == 0){
-        setTimeout(function(){waitload(callback);},100);
+extension.runtime.onMessage("checkFbCo", function(msg, sendResponse){
+    if($("input[type='password']").length==0){
+        sendResponse(true);
     } else {
-        callback();
+        sendResponse(false);
     }
-}
+});
 
-waitload(function(){
-    $(".fsl").click();
-    $("._4n9u.ellipsis").each(function(index){
-        console.log(index+". "+$(this).text());
+extension.runtime.onMessage("logoutFromFb", function(msg, sendResponse){
+    var domain = document.domain;
+    var path = "/";
+    document.cookie = "c_user=; expires=" + +new Date + "; domain=" + domain + "; path=" + path;
+    sendResponse();
+});
+
+extension.runtime.onMessage("connectToFb", function(msg, sendResponse){
+    if($("#email").length==0){
+        sendResponse(false);
+    } else {
+        $("#email").val(msg.login);
+        $("#pass").val(msg.pass);
+        $("#loginbutton").click();
+        sendResponse(true);
+    }
+});
+
+extension.runtime.onMessage("scrapFb", function(msg, sendResponse){
+    function waitload(callback){
+        if($("._ikh._4na3").length == 0){
+            setTimeout(function(){waitload(callback);},100);
+        } else {
+            callback();
+        }
+    }
+    var results=[];
+    waitload(function(){
+        $(".fsl").click();
+        var nbOfApps = $("._4n9u.ellipsis").length;
+        $("._4n9u.ellipsis").each(function(index){
+            if($(this).text().substr(0,3)=="Que" || $(this).text().substr(0,3)=="que"){
+                
+            }
+            results.push($(this).text());
+            if(index+1 == nbOfApps){
+                sendResponse(results);
+            }
+        });
+        
+        function clean(index, apps){
+            var app = $(apps[index]);
+            if(app.text().substr(0,3)=="Que" || app.text().substr(0,3)=="que"){
+                app.find("._25kn").click();
+                function waitElem(elem){
+                    if($(elem).length == 0){
+                        setTimeout(function(){waitElem(elem);},100);
+                    } else {
+                        
+                    }
+                }
+            }
+        }
     });
 });
